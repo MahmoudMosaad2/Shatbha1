17dc8b785b193c2cfed7746224c5d71117a5f3eb
MahmoudMosaad2 <87284919+MahmoudMosaad2@users.noreply.github.com>
2026-06-28 11:46:51 +0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ import {
   locationModifiers, 
   roleGuidelines, 
   engineeringSpecialties 
-} from "./server/shattabhaKnowledge";
+} from "./server/shattabhaKnowledge.ts";
 
 // Initialize environment variables
 dotenv.config();
